6d4ee19c9a10a5a95d4b07e890e5202e379ac86d
neighbors28 <sjp0728@gmail.com>
2014-05-09 17:11:10 +0200

    Frameworks/base: Second Clock (1/2)
    Dokdo-Project/platform_frameworks_base@6ab5d62#diff-1fa71c270d063a48f921f0760d10c7f3R122
    
    PS2: Fu Whitespace
    PS3: small Issue
    
    Change-Id: Ie04942ae1bb1649d18db51086b46666b56ef1ea0
    Signed-off-by: OwnDroid <askowndroid@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -45,7 +45,10 @@ import com.android.systemui.cm.UserContentObserver;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.TimeZone;
 
 import libcore.icu.LocaleData;
@@ -116,6 +119,9 @@ public class Clock extends TextView implements DemoMode {
             updateSettings();
         }
     }
+
+    private final Handler handler = new Handler();
+    TimerTask second;
 
     public Clock(Context context) {
         this(context, null);
@@ -249,6 +255,11 @@ public class Clock extends TextView implements DemoMode {
 
         String result = sdf.format(mCalendar.getTime());
 
+        if (Settings.System.getInt(mContext.getContentResolver(), Settings.System.CLOCK_USE_SECOND, 0) == 1) {
+            String temp = result;
+            result = String.format("%s:%02d", temp, new GregorianCalendar().get(Calendar.SECOND));
+        }
+
         if (mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE) {
             Date now = new Date();
 
@@ -330,6 +341,24 @@ public class Clock extends TextView implements DemoMode {
             // flag to reset the color
             clockColor = defaultColor;
         }
+
+        second = new TimerTask()
+        {
+            @Override
+            public void run()
+             {
+                Runnable updater = new Runnable()
+                  {
+                   public void run()
+                   {
+                       updateClock();
+                   }
+                  };
+                handler.post(updater);
+             }
+        };
+        Timer timer = new Timer();
+        timer.schedule(second, 0, 1001);
 
         getFontStyle(mClockFontStyle);
         setTextColor(clockColor);
