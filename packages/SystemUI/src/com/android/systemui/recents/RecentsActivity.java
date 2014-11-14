c993876de53db44d501bc994e72c97f1333440a4
Clark Scheff <clark@cyngn.com>
2014-11-14 09:44:51 -0800

    Add clear all recents action to recents panel
    Change-Id: I2243ebc83abc483e78b112900a653d7e61edc5ba

--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -252,6 +252,7 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                 }
             });
             mRecentsView.setSearchBarVisibility(View.GONE);
+            findViewById(R.id.clear_recents).setVisibility(View.GONE);
         } else {
             if (mEmptyView != null) {
                 mEmptyView.setVisibility(View.GONE);
@@ -259,7 +260,9 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
             }
             if (!mConfig.searchBarEnabled) {
                 mRecentsView.setSearchBarVisibility(View.GONE);
+                findViewById(R.id.clear_recents).setVisibility(View.GONE);
             } else {
+				findViewById(R.id.clear_recents).setVisibility(View.VISIBLE);
                 if (mRecentsView.hasValidSearchBar()) {
                     mRecentsView.setSearchBarVisibility(View.VISIBLE);
                 } else {
