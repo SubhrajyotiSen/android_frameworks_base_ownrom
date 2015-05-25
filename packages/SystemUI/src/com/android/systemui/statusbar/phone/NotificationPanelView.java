9c6ebc213be8737faee7d1a9058f4559f1f6ebff
Alex Cruz <mazdarider23@gmail.com>
2015-05-25 05:14:13 +0000

    Fixed Task Manager bug described in post below
    https://plus.google.com/101691799763674764197/posts/Z7XFJSfsCYz
    
    Change-Id: If4f3f3c67c80cd8be3111ea3dbc3ac1f3b6d7618

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1587,10 +1587,6 @@ public class NotificationPanelView extends PanelView implements
         mNotificationStackScroller.setScrollingEnabled(
                 mStatusBarState != StatusBarState.KEYGUARD && (!mQsExpanded
                         || mQsExpansionFromOverscroll));
-        if (Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.ENABLE_TASK_MANAGER, 0) == 1) {
-            mQsPanel.setVisibility(expandVisually ? View.VISIBLE : View.INVISIBLE);
-        }
         mQsContainer.setVisibility(
                 mKeyguardShowing && !expandVisually ? View.INVISIBLE : View.VISIBLE);
         mScrollView.setTouchEnabled(mQsExpanded);
