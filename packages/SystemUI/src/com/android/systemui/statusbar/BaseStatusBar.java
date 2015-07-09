4f0072517c7122b6453c4637b3b96ab82cd66fd0
Davor Bertovic <lordclockan@gmail.com>
2015-07-09 20:38:28 +0200

    Merge "[1/2] Base: option to launch heads up in floating mode." into lp5.1
    Change-Id: Ia526a36260bf1448348ab517af8194067b7e5427
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -111,6 +111,7 @@ import com.android.systemui.cm.SpamMessageProvider;
 import com.android.systemui.slimrecent.RecentController;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.AppSidebar;
+import com.android.systemui.statusbar.notification.NotificationHelper;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.phone.NavigationBarOverlay;
 import com.android.systemui.statusbar.phone.NavigationBarView;
@@ -326,6 +327,10 @@ public abstract class BaseStatusBar extends SystemUI implements
     public Handler getHandler() {
         return mHandler != null ? mHandler : createHandler();
     }
+
+    // Notification helper
+    protected NotificationHelper mNotificationHelper;
+
 
     protected final ContentObserver mSettingsObserver = new ContentObserver(mHandler) {
         @Override
@@ -680,6 +685,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         // Connect in to the status bar manager service
         StatusBarIconList iconList = new StatusBarIconList();
         mCommandQueue = new CommandQueue(this, iconList);
+
+        mNotificationHelper = new NotificationHelper(this, mContext);
 
         int[] switches = new int[8];
         ArrayList<IBinder> binders = new ArrayList<IBinder>();
@@ -1591,8 +1598,8 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         PendingIntent contentIntent = sbn.getNotification().contentIntent;
         if (contentIntent != null) {
-            final View.OnClickListener listener = makeClicker(contentIntent, sbn.getKey(),
-                    isHeadsUp);
+            final View.OnClickListener listener = mNotificationHelper.getNotificationClickListener
+                    (entry, isHeadsUp);
             row.setOnClickListener(listener);
         } else {
             row.setOnClickListener(null);
@@ -1761,15 +1768,20 @@ public abstract class BaseStatusBar extends SystemUI implements
         return new NotificationClicker(intent, notificationKey, forHun);
     }
 
-    protected class NotificationClicker implements View.OnClickListener {
+    public class NotificationClicker implements View.OnClickListener {
         private PendingIntent mIntent;
         private final String mNotificationKey;
         private boolean mIsHeadsUp;
+        public boolean mFloat;
 
         public NotificationClicker(PendingIntent intent, String notificationKey, boolean forHun) {
             mIntent = intent;
             mNotificationKey = notificationKey;
             mIsHeadsUp = forHun;
+        }
+
+        public void makeFloating(boolean floating) {
+            mFloat = floating;
         }
 
         public void onClick(final View v) {
@@ -1807,8 +1819,12 @@ public abstract class BaseStatusBar extends SystemUI implements
                             }
 
                             if (mIntent != null) {
+                                Intent overlay = new Intent();
+                                int flags = Intent.FLAG_FLOATING_WINDOW | Intent.FLAG_ACTIVITY_CLEAR_TASK;
+
+                            if (mFloat) overlay.addFlags(flags);
                                 try {
-                                    mIntent.send();
+                                    mIntent.send(mContext, 0, overlay);
                                 } catch (PendingIntent.CanceledException e) {
                                     // the stack trace isn't very helpful here.
                                     // Just log the exception message.
@@ -2305,8 +2321,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         // update the contentIntent
         final PendingIntent contentIntent = notification.getNotification().contentIntent;
         if (contentIntent != null) {
-            final View.OnClickListener listener = makeClicker(contentIntent, notification.getKey(),
-                    isHeadsUp);
+            final View.OnClickListener listener =
+                    mNotificationHelper.getNotificationClickListener(entry, isHeadsUp);
             entry.row.setOnClickListener(listener);
         } else {
             entry.row.setOnClickListener(null);
