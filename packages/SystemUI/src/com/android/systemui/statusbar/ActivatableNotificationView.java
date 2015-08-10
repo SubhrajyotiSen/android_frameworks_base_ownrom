XXMrHyde/android_frameworks_base

Notifications: Custom colors, (1/2): ("normal", heads up and lock screen notifications)
- Media background color mode:
  - Never colorize the background
  - Colorize the background if the notification doesn`t include a own color
  - Always colorize the background
- App icon background color mode:
  - Never colorize the background
  - Colorize the background if the notification doesn`t include a own color
  - Always colorize the background
- App icon color mode:
  - Never colorize the background
  - Colorize the icon if the icon is an greyscale icon
  - Always colorize the background
- Background color
- Background color, (on long click)
- App icon background color
- Text color
- Icon colors,
  (app-, action- and media action icons)

Fixes and additions:
- Use different colors for high/normal or minor priority
  (minor priority color is a darker/lighter color
  of the high/normal color)
- Don`t use greyscale icons when dozing
- Show the app icon background when dozing,
  (there is no color inverting, using a dark/black background
  will hide the background)

Known bugs:

Media notifications:
- Some media notifications doesn`t respect the choosen action icon color
  and text colors
  (e.g. PowerAmp. Notifications from Google Play Music and Eleven
  are working without any problems)

Notifications (general):
- Some notifications doesn`t respect a new text color,
  (if the notification is already present,
  new notifications are always using the "right" text color,
  e.g. GMail, Hangouts, ... seems it`s a similar issue like the one I fixed with
  the "Notifications: Fix dark text on dark background" commit,
  but unfortunately this fix doesn`t work in this case)

May be there are more bugs with notifications from other apps,
or scenarios I didn`t tested.

packages/SystemUI/src/com/android/systemui/statusbar/ActivatableNotificationView.java
@@ -40,6 +40,9 @@ import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.PathInterpolator;
 
+import com.android.internal.util.darkkat.ColorHelper;
+import com.android.internal.util.darkkat.NotificationColorHelper;
+
 import com.android.systemui.R;
 
 /**
@@ -135,8 +138,9 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
     private float mAppearAnimationTranslation;
     private boolean mShowingLegacyBackground;
     private final int mLegacyColor;
-    private final int mNormalColor;
-    private final int mLowPriorityColor;
+    private int mNormalColor;
+    private int mMediaColor;
+    private int mLowPriorityColor;
     private boolean mIsBelowSpeedBump;
 
     public ActivatableNotificationView(Context context, AttributeSet attrs) {
@@ -155,9 +159,6 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
         mRoundedRectCornerRadius = getResources().getDimensionPixelSize(
                 R.dimen.notification_material_rounded_rect_radius);
         mLegacyColor = getResources().getColor(R.color.notification_legacy_background_color);
-        mNormalColor = getResources().getColor(R.color.notification_material_background_color);
-        mLowPriorityColor = getResources().getColor(
-                R.color.notification_material_background_low_priority_color);
         mTintedRippleColor = context.getResources().getColor(
                 R.color.notification_ripple_tinted_color);
         mLowPriorityRippleColor = context.getResources().getColor(
@@ -174,7 +175,7 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
         mBackgroundNormal.setCustomBackground(R.drawable.notification_material_bg);
         mBackgroundDimmed.setCustomBackground(R.drawable.notification_material_bg_dim);
         updateBackground();
-        updateBackgroundTint();
+        updateSettings();
     }
 
     private final Runnable mTapTimeoutRunnable = new Runnable() {
@@ -384,16 +385,12 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
      */
     public void setTintColor(int color) {
         mBgTint = color;
-        updateBackgroundTint();
+        updateSettings();
     }
 
     private void updateBackgroundTint() {
         int color = getBackgroundColor();
         int rippleColor = getRippleColor();
-        if (color == mNormalColor) {
-            // We don't need to tint a normal notification
-            color = 0;
-        }
         mBackgroundDimmed.setTint(color);
         mBackgroundNormal.setTint(color);
         mBackgroundDimmed.setRippleColor(rippleColor);
@@ -668,7 +665,7 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
 
     private int getBackgroundColor() {
         if (mBgTint != 0) {
-            return mBgTint;
+            return mMediaColor;
         } else if (mShowingLegacyBackground) {
             return mLegacyColor;
         } else if (mIsBelowSpeedBump) {
@@ -749,5 +746,13 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
     public interface OnActivatedListener {
         void onActivated(ActivatableNotificationView view);
         void onActivationReset(ActivatableNotificationView view);
+    }
+
+    public void updateSettings() {
+        mNormalColor = NotificationColorHelper.getCustomNotificationBgColor(mContext);
+        mMediaColor = NotificationColorHelper.getNotificationMediaBgColor(mContext, mBgTint);
+        mLowPriorityColor = ColorHelper.getLightenOrDarkenColor(mNormalColor);
+
+        updateBackgroundTint();
     }
 }

SubhrajyotiSen/android_frameworks_base_ownrom

Change-Id: Ia625ba27cd417538869397ea7b1a662d0bfaf5

packages/SystemUI/src/com/android/systemui/statusbar/ActivatableNotificationView.java
@@ -40,6 +40,9 @@ import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.PathInterpolator;
 
+import com.android.internal.util.own.ColorHelper;
+import com.android.internal.util.own.NotificationColorHelper;
+
 import com.android.systemui.R;
 import com.android.systemui.ViewInvertHelper;
 
@@ -136,8 +139,9 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
     private float mAppearAnimationTranslation;
     private boolean mShowingLegacyBackground;
     private final int mLegacyColor;
-    private final int mNormalColor;
-    private final int mLowPriorityColor;
+    private int mNormalColor;
+    private int mMediaColor;
+    private int mLowPriorityColor;
     private boolean mIsBelowSpeedBump;
     private ViewInvertHelper mBackgroundNormalInvertHelper;
     private ViewInvertHelper mBackgroundDimmedInvertHelper;
@@ -159,9 +163,6 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
                 R.dimen.notification_material_rounded_rect_radius);
         setRoundCornerRadius(mRoundedRectCornerRadius); // Themes: For drop-shadow rounded corners
         mLegacyColor = getResources().getColor(R.color.notification_legacy_background_color);
-        mNormalColor = getResources().getColor(R.color.notification_material_background_color);
-        mLowPriorityColor = getResources().getColor(
-                R.color.notification_material_background_low_priority_color);
         mTintedRippleColor = context.getResources().getColor(
                 R.color.notification_ripple_tinted_color);
         mLowPriorityRippleColor = context.getResources().getColor(
@@ -182,7 +183,7 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
         mBackgroundDimmedInvertHelper =
                 new ViewInvertHelper(mBackgroundDimmed, DARK_ANIMATION_LENGTH);
         updateBackground();
-        updateBackgroundTint();
+        updateSettings();
     }
 
     private final Runnable mTapTimeoutRunnable = new Runnable() {
@@ -392,16 +393,12 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
      */
     public void setTintColor(int color) {
         mBgTint = color;
-        updateBackgroundTint();
+        updateSettings();
     }
 
     private void updateBackgroundTint() {
         int color = getBackgroundColor();
         int rippleColor = getRippleColor();
-        if (color == mNormalColor) {
-            // We don't need to tint a normal notification
-            color = 0;
-        }
         mBackgroundDimmed.setTint(color);
         mBackgroundNormal.setTint(color);
         mBackgroundDimmed.setRippleColor(rippleColor);
@@ -676,7 +673,7 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
 
     private int getBackgroundColor() {
         if (mBgTint != 0) {
-            return mBgTint;
+            return mMediaColor;
         } else if (mShowingLegacyBackground) {
             return mLegacyColor;
         } else if (mIsBelowSpeedBump) {
@@ -757,5 +754,13 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
     public interface OnActivatedListener {
         void onActivated(ActivatableNotificationView view);
         void onActivationReset(ActivatableNotificationView view);
+    }
+
+    public void updateSettings() {
+        mNormalColor = NotificationColorHelper.getCustomNotificationBgColor(mContext);
+        mMediaColor = NotificationColorHelper.getNotificationMediaBgColor(mContext, mBgTint);
+        mLowPriorityColor = ColorHelper.getLightenOrDarkenColor(mNormalColor);
+
+        updateBackgroundTint();
     }
 }