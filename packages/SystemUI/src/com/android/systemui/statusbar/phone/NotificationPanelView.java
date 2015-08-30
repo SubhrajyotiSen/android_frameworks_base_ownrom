e0858046656a216bf471ede052b5479ad9d1d7d3
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-08-30 21:03:49 +0100

    Reset to QS panel if task manager was visible last
    *also don't allow task manager to show on lock screen
    
    Change-Id: Iabb1a7a19d0b57c99c1c01fbeec2a3d7549e432f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1086,6 +1086,7 @@ public class NotificationPanelView extends PanelView implements
         if (action == MotionEvent.ACTION_DOWN && isFullyCollapsed()
                 && mQsExpansionEnabled) {
             mTwoFingerQsExpandPossible = true;
+            mStatusBar.resetQsPanelVisibility();
         }
         if (mTwoFingerQsExpandPossible && isOpenQsEvent(event)
                 && event.getY(event.getActionIndex()) < mStatusBarMinHeight) {
@@ -1624,6 +1625,7 @@ public class NotificationPanelView extends PanelView implements
         updateNotificationScrim(height);
         if (mKeyguardShowing) {
             updateHeaderKeyguard();
+            mStatusBar.resetQsPanelVisibility();
         }
         if (mStatusBarState == StatusBarState.KEYGUARD) {
             updateKeyguardBottomAreaAlpha();
@@ -1831,7 +1833,7 @@ public class NotificationPanelView extends PanelView implements
             mQsPanel.setVisibility(expandVisually && !taskManagerShowing
                     ? View.VISIBLE : View.GONE);
             mTaskManagerPanel.setVisibility(expandVisually && taskManagerShowing
-                    ? View.VISIBLE : View.GONE);
+                    && !mKeyguardShowing ? View.VISIBLE : View.GONE);
         }
     }
 
