35553f9504cf5cc0d36f9299154c323c1c92a41a
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-10-20 23:30:53 +0200

    Include color picker, (1/2)
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3832,6 +3832,16 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
+         * Colors used for the color picker panel puttons
+         * 0: OwnROM colors
+         * 1: Material colors
+         * 2: RGB colors
+         * default: 0
+         * @hide
+         */
+        public static final String COLOR_PICKER_PALETTE = "color_picker_palette";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
