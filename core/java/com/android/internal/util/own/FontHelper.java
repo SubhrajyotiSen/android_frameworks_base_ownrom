crdroidandroid/android_frameworks_base

Add Status Bar Ticker [1/2]
Change-Id: I4323720725e6f8826b74f4e72c88bbf0aecd40d1
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

core/java/com/android/internal/util/crdroid/FontHelper.java
@@ -1,0 +1,75 @@
+/*
+* Copyright (C) 2016 Cyanide Android (rogersb11)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.crdroid;
+
+import android.graphics.Typeface;
+
+public class FontHelper {
+
+    public static final int FONT_NORMAL = 0;
+    public static final int FONT_ITALIC = 1;
+    public static final int FONT_BOLD = 2;
+    public static final int FONT_BOLD_ITALIC = 3;
+    public static final int FONT_LIGHT = 4;
+    public static final int FONT_LIGHT_ITALIC = 5;
+    public static final int FONT_THIN = 6;
+    public static final int FONT_THIN_ITALIC = 7;
+    public static final int FONT_CONDENSED = 8;
+    public static final int FONT_CONDENSED_ITALIC = 9;
+    public static final int FONT_CONDENSED_LIGHT = 10;
+    public static final int FONT_CONDENSED_LIGHT_ITALIC = 11;
+    public static final int FONT_CONDENSED_BOLD = 12;
+    public static final int FONT_CONDENSED_BOLD_ITALIC = 13;
+    public static final int FONT_MEDIUM = 14;
+    public static final int FONT_MEDIUM_ITALIC = 15;
+    public static final int FONT_BLACK = 16;
+    public static final int FONT_BLACK_ITALIC = 17;
+    public static final int FONT_DANCINGSCRIPT = 18;
+    public static final int FONT_DANCINGSCRIPT_BOLD = 19;
+    public static final int FONT_COMINGSOON = 20;
+    public static final int FONT_NOTOSERIF = 21;
+    public static final int FONT_NOTOSERIF_ITALIC = 22;
+    public static final int FONT_NOTOSERIF_BOLD = 23;
+    public static final int FONT_NOTOSERIF_BOLD_ITALIC = 24;
+
+    public static final Typeface NORMAL = Typeface.create("sans-serif", Typeface.NORMAL);
+    public static final Typeface ITALIC = Typeface.create("sans-serif", Typeface.ITALIC);
+    public static final Typeface BOLD = Typeface.create("sans-serif", Typeface.BOLD);
+    public static final Typeface BOLD_ITALIC = Typeface.create("sans-serif", Typeface.BOLD_ITALIC);
+    public static final Typeface LIGHT = Typeface.create("sans-serif-light", Typeface.NORMAL);
+    public static final Typeface LIGHT_ITALIC = Typeface.create("sans-serif-light", Typeface.ITALIC);
+    public static final Typeface THIN = Typeface.create("sans-serif-thin", Typeface.NORMAL);
+    public static final Typeface THIN_ITALIC = Typeface.create("sans-serif-thin", Typeface.ITALIC);
+    public static final Typeface CONDENSED = Typeface.create("sans-serif-condensed", Typeface.NORMAL);
+    public static final Typeface CONDENSED_ITALIC = Typeface.create("sans-serif-condensed", Typeface.ITALIC);
+    public static final Typeface CONDENSED_LIGHT = Typeface.create("sans-serif-condensed-light", Typeface.NORMAL);
+    public static final Typeface CONDENSED_LIGHT_ITALIC = Typeface.create("sans-serif-condensed-light", Typeface.ITALIC);
+    public static final Typeface CONDENSED_BOLD = Typeface.create("sans-serif-condensed", Typeface.BOLD);
+    public static final Typeface CONDENSED_BOLD_ITALIC = Typeface.create("sans-serif-condensed", Typeface.BOLD_ITALIC);
+    public static final Typeface MEDIUM = Typeface.create("sans-serif-medium", Typeface.NORMAL);
+    public static final Typeface MEDIUM_ITALIC = Typeface.create("sans-serif-medium", Typeface.ITALIC);
+    public static final Typeface BLACK = Typeface.create("sans-serif-black", Typeface.NORMAL);
+    public static final Typeface BLACK_ITALIC = Typeface.create("sans-serif-black", Typeface.ITALIC);
+    public static final Typeface DANCINGSCRIPT = Typeface.create("cursive", Typeface.NORMAL);
+    public static final Typeface DANCINGSCRIPT_BOLD = Typeface.create("cursive", Typeface.BOLD);
+    public static final Typeface COMINGSOON = Typeface.create("casual", Typeface.NORMAL);
+    public static final Typeface NOTOSERIF = Typeface.create("serif", Typeface.NORMAL);
+    public static final Typeface NOTOSERIF_ITALIC = Typeface.create("serif", Typeface.ITALIC);
+    public static final Typeface NOTOSERIF_BOLD = Typeface.create("serif", Typeface.BOLD);
+    public static final Typeface NOTOSERIF_BOLD_ITALIC = Typeface.create("serif", Typeface.BOLD_ITALIC);
+
+}

core/java/com/android/internal/util/crdroid/TickerColorHelper.java
@@ -1,0 +1,42 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.crdroid;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.provider.Settings;
+
+public class TickerColorHelper {
+
+    private static final int WHITE = 0xffffffff;
+
+    public static int getTickerTextColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_TEXT_COLOR, WHITE);
+    }
+
+    public static ColorStateList getTickerIconColorList(Context context) {
+        return ColorStateList.valueOf(getTickerIconColor(context));
+    }
+
+    public static int getTickerIconColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_ICON_COLOR,
+                WHITE);
+    }
+}

SubhrajyotiSen/android_frameworks_base_ownrom

Signed-off-by: OwnDroid <owndroid26@gmail.com>

core/java/com/android/internal/util/own/FontHelper.java
@@ -1,0 +1,75 @@
+/*
+* Copyright (C) 2016 Cyanide Android (rogersb11)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.own;
+
+import android.graphics.Typeface;
+
+public class FontHelper {
+
+    public static final int FONT_NORMAL = 0;
+    public static final int FONT_ITALIC = 1;
+    public static final int FONT_BOLD = 2;
+    public static final int FONT_BOLD_ITALIC = 3;
+    public static final int FONT_LIGHT = 4;
+    public static final int FONT_LIGHT_ITALIC = 5;
+    public static final int FONT_THIN = 6;
+    public static final int FONT_THIN_ITALIC = 7;
+    public static final int FONT_CONDENSED = 8;
+    public static final int FONT_CONDENSED_ITALIC = 9;
+    public static final int FONT_CONDENSED_LIGHT = 10;
+    public static final int FONT_CONDENSED_LIGHT_ITALIC = 11;
+    public static final int FONT_CONDENSED_BOLD = 12;
+    public static final int FONT_CONDENSED_BOLD_ITALIC = 13;
+    public static final int FONT_MEDIUM = 14;
+    public static final int FONT_MEDIUM_ITALIC = 15;
+    public static final int FONT_BLACK = 16;
+    public static final int FONT_BLACK_ITALIC = 17;
+    public static final int FONT_DANCINGSCRIPT = 18;
+    public static final int FONT_DANCINGSCRIPT_BOLD = 19;
+    public static final int FONT_COMINGSOON = 20;
+    public static final int FONT_NOTOSERIF = 21;
+    public static final int FONT_NOTOSERIF_ITALIC = 22;
+    public static final int FONT_NOTOSERIF_BOLD = 23;
+    public static final int FONT_NOTOSERIF_BOLD_ITALIC = 24;
+
+    public static final Typeface NORMAL = Typeface.create("sans-serif", Typeface.NORMAL);
+    public static final Typeface ITALIC = Typeface.create("sans-serif", Typeface.ITALIC);
+    public static final Typeface BOLD = Typeface.create("sans-serif", Typeface.BOLD);
+    public static final Typeface BOLD_ITALIC = Typeface.create("sans-serif", Typeface.BOLD_ITALIC);
+    public static final Typeface LIGHT = Typeface.create("sans-serif-light", Typeface.NORMAL);
+    public static final Typeface LIGHT_ITALIC = Typeface.create("sans-serif-light", Typeface.ITALIC);
+    public static final Typeface THIN = Typeface.create("sans-serif-thin", Typeface.NORMAL);
+    public static final Typeface THIN_ITALIC = Typeface.create("sans-serif-thin", Typeface.ITALIC);
+    public static final Typeface CONDENSED = Typeface.create("sans-serif-condensed", Typeface.NORMAL);
+    public static final Typeface CONDENSED_ITALIC = Typeface.create("sans-serif-condensed", Typeface.ITALIC);
+    public static final Typeface CONDENSED_LIGHT = Typeface.create("sans-serif-condensed-light", Typeface.NORMAL);
+    public static final Typeface CONDENSED_LIGHT_ITALIC = Typeface.create("sans-serif-condensed-light", Typeface.ITALIC);
+    public static final Typeface CONDENSED_BOLD = Typeface.create("sans-serif-condensed", Typeface.BOLD);
+    public static final Typeface CONDENSED_BOLD_ITALIC = Typeface.create("sans-serif-condensed", Typeface.BOLD_ITALIC);
+    public static final Typeface MEDIUM = Typeface.create("sans-serif-medium", Typeface.NORMAL);
+    public static final Typeface MEDIUM_ITALIC = Typeface.create("sans-serif-medium", Typeface.ITALIC);
+    public static final Typeface BLACK = Typeface.create("sans-serif-black", Typeface.NORMAL);
+    public static final Typeface BLACK_ITALIC = Typeface.create("sans-serif-black", Typeface.ITALIC);
+    public static final Typeface DANCINGSCRIPT = Typeface.create("cursive", Typeface.NORMAL);
+    public static final Typeface DANCINGSCRIPT_BOLD = Typeface.create("cursive", Typeface.BOLD);
+    public static final Typeface COMINGSOON = Typeface.create("casual", Typeface.NORMAL);
+    public static final Typeface NOTOSERIF = Typeface.create("serif", Typeface.NORMAL);
+    public static final Typeface NOTOSERIF_ITALIC = Typeface.create("serif", Typeface.ITALIC);
+    public static final Typeface NOTOSERIF_BOLD = Typeface.create("serif", Typeface.BOLD);
+    public static final Typeface NOTOSERIF_BOLD_ITALIC = Typeface.create("serif", Typeface.BOLD_ITALIC);
+
+}

core/java/com/android/internal/util/own/TickerColorHelper.java
@@ -1,0 +1,42 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.own;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.provider.Settings;
+
+public class TickerColorHelper {
+
+    private static final int WHITE = 0xffffffff;
+
+    public static int getTickerTextColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_TEXT_COLOR, WHITE);
+    }
+
+    public static ColorStateList getTickerIconColorList(Context context) {
+        return ColorStateList.valueOf(getTickerIconColor(context));
+    }
+
+    public static int getTickerIconColor(Context context) {
+        return Settings.System.getInt(context.getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_ICON_COLOR,
+                WHITE);
+    }
+}